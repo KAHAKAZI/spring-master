9642662c5dd67c180f2337f14f48c3adf513a012
kahakazi <kahakazi@gmail.com>
2020-12-08 21:57:28 +0100

    Adds basic payment implementation

---
+++ src/main/java/pl/training/shop/payments/Payment.java
@@ -0,0 +1,20 @@
+package pl.training.shop.payments;
+
+import lombok.Builder;
+import lombok.Value;
+import org.javamoney.moneta.FastMoney;
+
+import java.time.Instant;
+
+@Builder
+@Value
+public class Payment {
+
+    String id;
+    FastMoney moeny;
+    Instant timestamp;
+    PaymentStatus status;
+
+
+
+}
