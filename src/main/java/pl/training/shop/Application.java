56c85995940df5a4f1b6c182fbf9237aaf0ff3ee
kahakazi <kahakazi@gmail.com>
2021-01-14 21:52:18 +0100

    Application scope, init and destroy methods

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -11,7 +11,8 @@ public class Application {
     private static final String BASE_PACKAGE = "pl.training.shop";
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BASE_PACKAGE)) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BASE_PACKAGE);
+//        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BASE_PACKAGE)) {
             var paymentService = applicationContext.getBean(PaymentService.class);
             var paymentRequest = PaymentRequest.builder()
                     .money(LocalMoney.of(1_000))
@@ -19,6 +20,8 @@ public class Application {
 
             var payment = paymentService.process(paymentRequest);
             log.info(payment.toString());
+
+            applicationContext.close();
         }
     }
-}
+//}
--- src/main/java/pl/training/shop/payments/FakePaymentService.java
+++ src/main/java/pl/training/shop/payments/FakePaymentService.java
@@ -5,11 +5,19 @@ import lombok.Setter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.time.Instant;
 
+@Log
+//@Scope("singleton") // singleton ins default, does not have to be specified
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Service("paymentService")
 //@RequiredArgsConstructor
 public class FakePaymentService implements PaymentService {
@@ -33,6 +41,17 @@ public class FakePaymentService implements PaymentService {
                 .status(PaymentStatus.STARTED)
                 .build();
         return paymentRepository.save(payment);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("PaymentService init");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("PaymentService is going down");
+
     }
 
 }
