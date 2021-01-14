98fb93f2583cf613bb4bbf4eceb6230a91eaef4a
kahakazi <kahakazi@gmail.com>
2021-01-14 21:31:12 +0100

    DI by custom annotation name

--- src/main/java/pl/training/shop/payments/FakePaymentService.java
+++ src/main/java/pl/training/shop/payments/FakePaymentService.java
@@ -18,7 +18,7 @@ public class FakePaymentService implements PaymentService {
     private PaymentRepository paymentRepository;
 
     @Autowired
-    public FakePaymentService(@Qualifier("incremental") PaymentIdGenerator paymentIdGenerator, PaymentRepository paymentRepository) {
+    public FakePaymentService(@IdGenerator("uuid") PaymentIdGenerator paymentIdGenerator, PaymentRepository paymentRepository) {
         this.paymentIdGenerator = paymentIdGenerator;
         this.paymentRepository = paymentRepository;
     }
